371ddac24ef11e737c727d9ee3456f4a3725df2b
Oleksandr Bondar <aleksandrr.bondar@gmail.com>
2023-12-03 19:40:06 +0200

    Refactoring snake movement

--- app/src/main/java/com/flowers/mapEntity/Snake.java
+++ app/src/main/java/com/flowers/mapEntity/Snake.java
@@ -26,23 +26,16 @@ public class Snake extends View {
     }
 
     public class Node extends View {
-        public float targetX;
-        public float targetY;
-        public float posX;
-        public float posY;
-        public Node prevNode;
-
-        public void setPosX(float newPos) {
-            posX = newPos;
-        }
-
-        public void setPosY(float newPos) {
-            posY = newPos;
-        }
-
+        private Node prevNode;
         private Bitmap bitmap;
-
+        private float posX, posY;
         private NodeType nodeType;
+        private float targetX, targetY;
+
+        public void setPos(float newPosX, float newPosY) {
+            posX = newPosX;
+            posY = newPosY;
+        }
 
         public void setBitMap(int id) {
             bitmap = BitmapFactory.decodeResource(getResources(), id);
@@ -53,17 +46,8 @@ public class Snake extends View {
             super(context);
 
             nodeType = type;
-
-            if (type == NodeType.Head) {
-                setBitMap(R.drawable.snake_head);
-                tryRandomizeDirection();
-            } else {
-                setBitMap(R.drawable.body);
-            }
-        }
-
-        public NodeType nodeType() {
-            return nodeType;
+            setBitMap(type == NodeType.Head ? R.drawable.snake_head : R.drawable.body);
+            tryRandomizeDirection();
         }
 
         public void draw(Canvas canvas) {
@@ -71,42 +55,36 @@ public class Snake extends View {
             canvas.drawBitmap(bitmap, posX, posY, null);
         }
 
-        public float vectorLen(float x, float y)
-        {
-            return (float)sqrt(x * x + y * y);
+        public float vectorLen(float x, float y) {
+            return (float) sqrt(x * x + y * y);
         }
 
         private void updatePosition() {
-            if (prevNode != null && vectorLen(posX - prevNode.posX, posY - prevNode.posY) < 110.f)
-            {
-
-            }
-            else
-            {
-                float tX = targetX;
-                float tY = targetY;
-                if (prevNode != null)
-                {
-                    tX = prevNode.posX;
-                    tY = prevNode.posY;
-                }
-
-                float directionX = tX - posX;
-                float directionY = tY - posY;
-                if (equals(directionX, 0.0f, (float) (speed * 1.2)) && equals(directionY, 0.0f, (float) (speed * 1.2))) {
-                    tryRandomizeDirection();
-                    directionX = tX - posX;
-                    directionY = tY - posY;
-                }
-                directionX /= vectorLen(directionX, directionY);
-                directionY /= vectorLen(directionX, directionY);
-
-                posX += directionX * speed;
-                posY += directionY * speed;
-
-                detectCollisionWithBorderMap();
+            if (prevNode != null && vectorLen(posX - prevNode.posX, posY - prevNode.posY) < 110.f) {
+                return;
             }
 
+            float tX = (prevNode != null) ? prevNode.posX : targetX;
+            float tY = (prevNode != null) ? prevNode.posY : targetY;
+
+            float directionX = tX - posX;
+            float directionY = tY - posY;
+            float len = vectorLen(directionX, directionY);
+
+            if (equals(len, 0.0f, speed * 1.2f)) {
+                tryRandomizeDirection();
+                directionX = tX - posX;
+                directionY = tY - posY;
+                len = vectorLen(directionX, directionY);
+            }
+
+            directionX /= len;
+            directionY /= len;
+
+            posX += directionX * speed;
+            posY += directionY * speed;
+
+            detectCollisionWithBorderMap();
             postInvalidate();
         }
 
@@ -115,26 +93,24 @@ public class Snake extends View {
         }
 
         private void detectCollisionWithBorderMap() {
-
             if (nodeType == NodeType.Body) {
                 return;
             }
 
-            if (posX < 0) {
-                posX = 0;
-                tryRandomizeDirection();
-            } else if (posX > GameState.getInstance().mapWidth() - bitmap.getWidth()) {
-                posX = GameState.getInstance().mapWidth() - bitmap.getWidth();
-                tryRandomizeDirection();
-            }
-            if (posY < 0) {
-                posY = 0;
-                tryRandomizeDirection();
-            } else if (posY > GameState.getInstance().mapHeight() - bitmap.getHeight()) {
-                posY = GameState.getInstance().mapHeight() - bitmap.getHeight();
+            final float mapWidth = GameState.getInstance().mapWidth() - bitmap.getWidth();
+            final float mapHeight = GameState.getInstance().mapHeight() - bitmap.getHeight();
+
+            if (posX < 0 || posY < 0 || posX > mapWidth || posY > mapHeight) {
+                if (posX > mapWidth) {
+                    posX = mapWidth;
+                }
+                if (posY > mapHeight) {
+                    posY = mapHeight;
+                }
                 tryRandomizeDirection();
             }
         }
+
         private void tryRandomizeDirection() {
             if (nodeType == NodeType.Head) {
                 Random random = new Random(System.currentTimeMillis());
@@ -144,61 +120,62 @@ public class Snake extends View {
         }
     }
 
-    private ArrayList<Node> nodes = new ArrayList<>();
-    private float posX, posY;
     private final float speed = 5;
+    private ArrayList<Node> nodes = new ArrayList<>();
 
     public Snake(Context context) {
         super(context);
 
-        addNode(NodeType.Head, null);
-        addNode(NodeType.Body,nodes.get(0));
-        addNode(NodeType.Body,nodes.get(1));
-        addNode(NodeType.Body,nodes.get(2));
-        addNode(NodeType.Body,nodes.get(3));
-        addNode(NodeType.Body,nodes.get(4));
+        addNode(NodeType.Head);
+        addNode(NodeType.Body);
 
-        Timer timer = new Timer();
+        Timer timerUpdate = new Timer();
 
-        timer.schedule(new TimerTask() {
+        increaseSnakeNode();
+        timerUpdate.schedule(new TimerTask() {
             @Override
             public void run() {
-                for (Node node : nodes) {
-                    node.updatePosition();
-                    postInvalidate();
+                synchronized (nodes) {
+                    for (Node node : nodes) {
+                        node.updatePosition();
+                    }
                 }
+                postInvalidate();
             }
         }, 0, 10);
+    }
+
+    private void increaseSnakeNode() {
+        Timer timerAdd = new Timer();
+
+        timerAdd.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                synchronized (nodes) {
+                    addNode(NodeType.Body);
+                }
+            }
+        }, 0, 3000);
     }
 
     @SuppressLint("DrawAllocation")
     @Override
     protected void onDraw(Canvas canvas) {
-        for (Node node : nodes) {
+        for (Node node : nodes)
             node.draw(canvas);
-        }
     }
 
-    public void addNode(NodeType type, Node prev) {
+    public void addNode(NodeType type) {
         Node node = new Node(GameState.getInstance().getGameActivity(), type);
 
-        if (prev != null) {
-            node.prevNode = prev;
+        if (!nodes.isEmpty())
+            node.prevNode = nodes.get(nodes.size() - 1);
+        if (type == NodeType.Head) {
+            Random rand = new Random();
+            node.setPos(rand.nextInt(GameState.getInstance().mapWidth()), rand.nextInt(GameState.getInstance().mapHeight()));
+        } else {
+            node.setPos(node.prevNode.posX, node.prevNode.posY);
         }
-        if (type == NodeType.Head)
-        {
-            setRandomPos();
-        }
-        node.setPosX(posX);
-        node.setPosY(posY);
-
         nodes.add(node);
-    }
-
-    private void setRandomPos()
-    {
-        Random rand = new Random();
-        posX = rand.nextInt(GameState.getInstance().mapWidth());
-        posY = rand.nextInt(GameState.getInstance().mapHeight());
     }
 }
